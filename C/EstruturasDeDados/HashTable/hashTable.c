c406ccca68fa663d93068c005bfc4e6a2d2b3e35
FelipeFioreti <felipe.fioretirosolen@gmail.com>
2025-05-22 17:45:24 -0300

    Adicionando Hash Table

--- C/EstruturasDeDados/HashTable/hashTable.c
+++ C/EstruturasDeDados/HashTable/hashTable.c
@@ -6,22 +6,25 @@
 #define TAMANHO 26
 #define TAM_MAX_NOME 100
 
-// Estrutura de um nó da lista
 typedef struct Table {
     char chave[TAM_MAX_NOME];
-    struct No* proximo;
+    struct Table* proximo;
 } Table;
 
-// A tabela hash: array de ponteiros para listas (uma para cada letra)
 Table* hashTable[TAMANHO];
 
-// Função hash: mapeia a primeira letra para um índice de 0 a 25
+void converterParaMaiusculo(char *nome) {
+    for (int i = 0; nome[i]; i++) {
+        nome[i] = toupper(nome[i]);
+    }
+}
+
 int hash(char c) {
-    c = tolower(c); // considerar letras minúsculas
+    c = tolower(c); 
     if (c >= 'a' && c <= 'z') {
         return c - 'a';
     }
-    return -1; // caractere inválido
+    return -1; 
 }
 
 Table * novoElemento(char* nome){
@@ -31,7 +34,6 @@ Table * novoElemento(char* nome){
     return novo;
 }
 
-// Inserir nome na hash table
 void inserir(char* nome) {
     int indice = hash(nome[0]);
     if (indice == -1) {
@@ -40,31 +42,158 @@ void inserir(char* nome) {
     }
 
     Table * novo = novoElemento(nome);
-
+    
     if (hashTable[indice] == NULL) {
         hashTable[indice] = novo;
-    } else {
-        
-        novo->proximo = hashTable[indice];
+        return;
+    }
+    
+    if (strcmp(novo -> chave, hashTable[indice] -> chave) == 0) {
+        printf("Nao podem existir valores repetidos em uma Hash Table.\n");
+        free(novo); 
+        return;
+    }
+
+    
+    if(strcmp(novo -> chave, hashTable[indice] -> chave) < 0){
+        novo -> proximo = hashTable[indice];
         hashTable[indice] = novo;
+        return;
     }
-}
 
-// Imprimir a tabela
-void imprimirTabela() {
-    for (int i = 0; i < TAMANHO; i++) {
-        printf("[%c]: ", i + 'A');
-        Table* atual = hashTable[i];
-        while (atual != NULL) {
-            printf("%s -> ", atual->chave);
-            atual = atual->proximo;
+    Table * auxiliar = hashTable[indice];
+    Table * corrente = auxiliar -> proximo;
+
+    while (corrente != NULL)
+    {
+        if(strcmp(corrente -> chave, novo -> chave) == 0){
+            printf("Nao podem existir valores repetidos em uma Hash Table. \n");
+            free(novo); 
+            return;
         }
-        printf("NULL\n");
+
+        if(strcmp(corrente -> chave, novo -> chave) > 0){
+            break;
+        }
+
+        auxiliar = corrente;
+        corrente = corrente -> proximo;
+    }
+
+    if(corrente == NULL){
+        auxiliar -> proximo = novo;
+        novo -> proximo = NULL;
+    }else{
+        auxiliar -> proximo = novo;
+        novo -> proximo = corrente;
+    }
+
+}
+
+
+void imprimirHashTable() {
+    for (int i = 0; i < TAMANHO; i++) {
+
+        if (hashTable[i]) {
+
+            printf("[%c]: ", i + 'A');
+            
+            Table* corrente = hashTable[i];
+            
+            while (corrente != NULL) {
+            
+                printf("%s -> ", corrente->chave);
+                corrente = corrente->proximo;
+
+            }
+
+            printf("NULL\n");
+        }
     }
 }
 
-// Liberar memória
-void liberarTabela() {
+void pesquisarHashTable(char* nome){
+    int indice = hash(nome[0]);
+
+    if (indice == -1) {
+        printf("Caractere inválido: %s\n", nome);
+        return;
+    }
+
+    if(hashTable[indice] == NULL){
+        printf("Nenhum nome na Hash Table comeca com a letra %c \n", nome[0]);
+    }else{
+
+        Table * corrente = hashTable[indice];
+        int achou = 0;
+
+        while(corrente != NULL){
+            if(strcmp(corrente -> chave, nome) == 0){
+                achou = 1;
+                break;
+            }
+
+            corrente = corrente -> proximo;
+        }
+
+        if(achou){
+            printf("Nome encontrado. \n");
+        }else{
+            printf("Nome não foi encontrado. \n");
+        }
+
+    }
+
+}
+
+void removerHashTable(char * nome){
+   int indice = hash(nome[0]);
+
+    if (indice == -1) {
+        printf("Caractere inválido: %s\n", nome);
+        return;
+    }
+
+    if(hashTable[indice] == NULL){
+        printf("Nenhum nome na Hash Table comeca com a letra %c \n", nome[0]);
+    }else{
+        
+        Table * corrente = hashTable[indice];
+
+        if (strcmp(hashTable[indice] -> chave, nome) == 0) {
+            hashTable[indice] = corrente -> proximo;
+            free(corrente);
+            printf("Nome removido com sucesso.\n");
+            return;
+        }
+
+        Table * auxiliar = corrente;
+        corrente = corrente -> proximo;
+        int achou = 0;
+
+        while(corrente != NULL){
+            if(strcmp(corrente -> chave, nome) == 0){
+                auxiliar -> proximo = corrente -> proximo;
+                free(corrente);
+                achou = 1;
+                break;
+            }
+
+            auxiliar = corrente;
+            corrente = corrente -> proximo;
+        }
+
+        if(achou){
+            printf("Nome removido com sucesso.\n");
+        }else{
+            printf("Nome não foi encontrado.\n");
+        }
+
+    }
+
+}
+
+void liberarHashTable() {
     for (int i = 0; i < TAMANHO; i++) {
         Table* atual = hashTable[i];
         while (atual != NULL) {
@@ -76,15 +205,107 @@ void liberarTabela() {
 }
 
 int main() {
-    inserir("Ana");
-    inserir("Amanda");
-    inserir("Bruno");
-    inserir("Carlos");
-    inserir("alice");
-    inserir("bob");
 
-    imprimirTabela();
+    for(int i = 0; i < TAMANHO; i++){
+        hashTable[i] = NULL;
+    }
 
-    liberarTabela();
+    int opcaoMenu = 0;
+
+    // Inserir
+    char nomeInserir[TAM_MAX_NOME];   
+    char respostaInserir = 'S';
+
+    // Pesquisar
+    char nomePesquisar[TAM_MAX_NOME];
+
+    // Remover
+    char nomeRemover[TAM_MAX_NOME];
+
+    do{
+
+        system("cls");
+        system("pause");
+        printf("\n [ 1 ] Inserir");
+        printf("\n [ 2 ] Visualizar");
+        printf("\n [ 3 ] Pesquisar");
+        printf("\n [ 4 ] Remover");
+        printf("\n [ 5 ] Sair");
+        printf("\n\nInforme a opcao [ ]");
+        scanf("%d", &opcaoMenu);
+
+        switch (opcaoMenu)
+        {
+            case 1:
+                do {
+                    printf("\nInforme o nome: \n");
+                    
+                    fflush(stdin);
+                    fgets(nomeInserir, TAM_MAX_NOME, stdin);
+                    fflush(stdin);
+
+                    nomeInserir[strcspn(nomeInserir, "\n")] = '\0';
+
+                    converterParaMaiusculo(nomeInserir);
+
+                    inserir(nomeInserir);
+
+                    printf("Quer continuar a inserir? (S/N): \n");
+                    scanf(" %c", &respostaInserir);
+                    respostaInserir = toupper(respostaInserir);
+
+                } while (respostaInserir != 'N');
+                break;
+            case 2:
+                imprimirHashTable();
+                system("pause");
+                break;
+
+            case 3:
+                printf("Informe o nome que quer pesquisar: \n");
+                
+                fflush(stdin);
+                fgets(nomePesquisar, TAM_MAX_NOME, stdin);
+                fflush(stdin);
+
+                nomePesquisar[strcspn(nomePesquisar, "\n")] = '\0';
+
+                converterParaMaiusculo(nomePesquisar);
+
+                pesquisarHashTable(nomePesquisar);
+
+                system("pause");
+                break;
+
+            case 4:
+                printf("Informe o nome que quer remover: \n");
+                
+                fflush(stdin);
+                fgets(nomeRemover, TAM_MAX_NOME, stdin);
+                fflush(stdin);
+
+                nomeRemover[strcspn(nomeRemover, "\n")] = '\0';
+
+                converterParaMaiusculo(nomeRemover);
+
+                removerHashTable(nomeRemover);
+
+                system("pause");
+                break;
+
+            case 5:
+                printf("\nSaindo...\n");
+                system("pause");
+                liberarHashTable();
+                exit(0);
+                break;
+
+            default :
+                printf("\nOpcao invalida, tente novamente \n");
+                system("pause");
+                break;
+        }
+    }while(opcaoMenu != 5);
+
     return 0;
 }
